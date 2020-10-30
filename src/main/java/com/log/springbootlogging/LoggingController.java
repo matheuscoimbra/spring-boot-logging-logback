f1b2ae46da673f5c845cb9c3c4c88cf2bee359a4
matheus <matheus.moraes@hospitalsaodomingos.com.br>
2020-10-30 14:28:50 -0300

    tr

--- src/main/java/com/log/springbootlogging/LoggingController.java
+++ src/main/java/com/log/springbootlogging/LoggingController.java
@@ -1,18 +1,18 @@
 package com.log.springbootlogging;
 
 import com.log.springbootlogging.service.LogService;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@AllArgsConstructor
 @RestController
 public class LoggingController {
 
-    @Autowired
-    private LogService service;
 
-    @Autowired
-    private LogService2 service2;
+    private final LogService service;
+
+    private final LogService2 service2;
 
     @GetMapping("/")
     public String index() {
